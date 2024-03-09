cb3230ac85903bb5f4a24da79a4eeacff5918a9c
ありす <master@16439s.jp>
2024-03-09 21:36:15 +0900

    Revert "Feat: PINGのときにレイテンシーをはかるようにする。"
    This reverts commit 343fa29da6d93e8ceedbf69455bfd6c787be3c46.

--- src/modules/ping/index.ts
+++ src/modules/ping/index.ts
@@ -3,27 +3,24 @@ import Module from '@/module.js';
 import Message from '@/message.js';
 
 export default class extends Module {
-    public readonly name = 'ping';
+	public readonly name = 'ping';
 
-    @bindThis
-    public install() {
-        return {
-            mentionHook: this.mentionHook
-        };
-    }
+	@bindThis
+	public install() {
+		return {
+			mentionHook: this.mentionHook
+		};
+	}
 
-    @bindThis
-    private async mentionHook(msg: Message) {
-        if (msg.text && msg.text.includes('ping')) {
-            const startTime = process.hrtime();
-            
-            const endTime = process.hrtime(startTime);
-            const latency = Math.round((endTime[0] * 1000) + (endTime[1] / 1000000)); // レイテンシーをミリ秒に変換
-
-            msg.reply(`応答完了 レイテンシーは ${latency}ms でした。`);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	@bindThis
+	private async mentionHook(msg: Message) {
+		if (msg.text && msg.text.includes('ping')) {
+			msg.reply('PONG!', {
+				immediate: true
+			});
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
